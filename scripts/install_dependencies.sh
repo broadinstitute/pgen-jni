a9fad2776b76af9eaf031381a811d4fbeac197a8
Chris Norman <cnorman@broadinstitute.org>
2023-01-25 11:21:21 -0500

    Suppress wget output from surfacing in public github action logs.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,10 +2,12 @@
 # install plink2 for roundtrip testing
 set -ex
 cd scripts
-wget https://s3.amazonaws.com/plink2-assets/alpha3/plink2_linux_avx2_20221024.zip -P scripts
+# use wget -q to supress wget from emitting aws keys into public logs
+wget -q https://s3.amazonaws.com/plink2-assets/alpha3/plink2_linux_avx2_20221024.zip -P scripts
 unzip scripts/plink2_linux_avx2_20221024.zip -d scripts
 mv scripts/plink2 /usr/local/bin
 # install boost 1.8.0 for tests
-wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz -P scripts
+wget -q https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz -P scripts
 tar xf scripts/boost_1_80_0.tar.gz -C scripts
 sudo mv scripts/boost_1_80_0 /usr/local/boost
+
