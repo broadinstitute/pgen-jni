fac4168f5849d3e943264714e2ae2111ce5b6755
Chris Norman <cnorman@broadinstitute.org>
2023-02-22 14:46:40 -0500

    Update plink source scripts and record most recent commit.

---
+++ README_plink_commit_hash.md
@@ -0,0 +1,7 @@
+The plink-ng source in this repo is from plink-ng commit:
+
+commit c18bc77ea2a5fde71442b83a2f46b66f80563ac1
+Author: Christopher Chang <chrchang523@gmail.com>
+Date:   Mon Jan 16 21:29:52 2023 -0800
+
+    dev rebuild
--- scripts/updatePlinkCode.sh
+++ scripts/updatePlinkCode.sh
@@ -4,6 +4,7 @@
 cp ../plink-ng/2.0/include/plink2_base.h ./pgen-lib/src/main/headers
 cp ../plink-ng/2.0/include/plink2_bits.h ./pgen-lib/src/main/headers
 cp ../plink-ng/2.0/include/pgenlib_misc.h ./pgen-lib/src/main/headers
+cp ../plink-ng/2.0/include/pgenlib_read.h ./pgen-lib/src/main/headers
 cp ../plink-ng/2.0/include/pgenlib_write.h ./pgen-lib/src/main/headers
 cp ../plink-ng/2.0/pgenlib_ffi_support.h ./pgen-lib/src/main/headers
 
@@ -11,7 +12,11 @@ cp ../plink-ng/2.0/pgenlib_ffi_support.h ./pgen-lib/src/main/headers
 cp ../plink-ng/2.0/include/plink2_base.cc ./pgen-lib/src/main/cpp
 cp ../plink-ng/2.0/include/plink2_bits.cc ./pgen-lib/src/main/cpp
 cp ../plink-ng/2.0/include/pgenlib_misc.cc ./pgen-lib/src/main/cpp
-cp ../plink-ng/2.0/include/pgenlib_read.cc ./pgen-lib/src/main/cpp/pgenlib_read.cc
+cp ../plink-ng/2.0/include/pgenlib_read.cc ./pgen-lib/src/main/cpp
 cp ../plink-ng/2.0/include/pgenlib_write.cc ./pgen-lib/src/main/cpp
-cp ../plink-ng/2.0/pgenlib_ffi_support.cc ./pgen-lib/src/main/cpp/pgenlib_ffi_support.cc
+cp ../plink-ng/2.0/pgenlib_ffi_support.cc ./pgen-lib/src/main/cpp
+
+# record the commit hash of this update
+echo "The plink-ng source in this repo is from plink-ng commit:\n" > README_plink_commit_hash.md
+git -C ../plink-ng/ log -1 >> README_plink_commit_hash.md
 
