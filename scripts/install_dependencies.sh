fd30f1a6cd5cc380bdd3e246e355ce01425a0b82
Chris Norman <cnorman@broadinstitute.org>
2023-04-17 09:38:00 -0400

    Update to a newer plink2 executable for use in tests, add it to the Docker.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+
+# NOTE: The versions of plink2 and boost used here and in the Dockerfile should be kept in sync.
+
 # install plink2 for roundtrip testing
 set -ex
 cd scripts
 # use wget -q to supress wget from emitting aws keys into public logs
-wget -q https://s3.amazonaws.com/plink2-assets/alpha3/plink2_linux_avx2_20221024.zip -P scripts
-unzip scripts/plink2_linux_avx2_20221024.zip -d scripts
-mv scripts/plink2 /usr/local/bin
+wget -q https://s3.amazonaws.com/plink2-assets/plink2_linux_avx2_20230411.zip -P scripts
+unzip scripts/plink2_linux_avx2_20230411.zip -d scripts
+sudo mv scripts/plink2 /usr/local/bin
+
 # install boost 1.8.0 for tests
 wget -q https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz -P scripts
 tar xf scripts/boost_1_80_0.tar.gz -C scripts
 sudo mv scripts/boost_1_80_0 /usr/local/boost
-
